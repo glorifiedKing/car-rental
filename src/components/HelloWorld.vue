4dfd230e2668e10cd66023285b4084664a295826
Raymond <rayim25@gmil.com>
2019-08-23 07:01:13 -0700

    home starter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,87 +1,117 @@
+/* eslint-disable */
 <template>
-  <v-container>
-    <v-layout
-      text-center
-      wrap
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
     >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/car-rental.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.text"
+          @click=""
+        >
+          <v-list-item-action>
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to MotPort
-        </h1>
-        <p class="subheading font-weight-regular">
-          This is the test page,
-          <br>wooh
-          <a href="https://testcarental.ml" target="_blank">backend site</a>
-        </p>
-      </v-flex>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-subheader class="mt-4 grey--text text--darken-1">SUB</v-subheader>
+        <v-list>
+          <v-list-item
+            v-for="item in items2"
+            :key="item.text"
+            @click=""
+          >
+            <v-list-item-avatar>
+              <img
+                :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`"
+                alt=""
+              >
+            </v-list-item-avatar>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
 
+      </v-list>
+    </v-navigation-drawer>
 
-    </v-layout>
-  </v-container>
+    <v-app-bar
+      app
+      clipped-left
+      color="red"
+      dense
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-icon class="mx-4">fab fa-youtube</v-icon>
+      <v-toolbar-title class="mr-12 align-center">
+        <span class="title">Chariot</span>
+      </v-toolbar-title>
+      <div class="flex-grow-1"></div>
+
+    </v-app-bar>
+
+    <v-content>
+      <v-container >
+        <v-row>
+          <v-card
+           color="#385F73"
+           dark
+         >
+           <v-card-text class="white--text">
+             <div class="headline mb-2">Pick from</div>
+             ....
+           </v-card-text>
+
+           <v-card-actions>
+             <v-btn text>...</v-btn>
+           </v-card-actions>
+         </v-card>
+
+        </v-row>
+        <v-row>
+          <v-card
+           color="#385F73"
+           dark
+         >
+           <v-card-text class="white--text">
+             <div class="headline mb-2">Pick from</div>
+             ....
+           </v-card-text>
+
+           <v-card-actions>
+             <v-btn text>...</v-btn>
+           </v-card-actions>
+         </v-card>
+
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'trending_up', text: 'Most Popular' },
+        { icon: 'subscriptions', text: 'Subscriptions' },
+      ],
+      items2: [
+
+      ],
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
 </script>
